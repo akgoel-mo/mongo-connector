ff4b657daac86df51b9f4e42f0d6bbf5fdc40632
Akshay Goel <akshay@moengage.com>
2016-05-18 20:14:53 +0530

    Added field name prefix to documents

--- mongo_connector/doc_managers/formatters.py
+++ mongo_connector/doc_managers/formatters.py
@@ -6,13 +6,13 @@ from uuid import UUID
 from math import isnan, isinf
 
 import logging
-LOG = logging.getLogger(__name__)
 
-
+from enum import Enum
 import bson
 import bson.json_util
 
 from mongo_connector.compat import PY3
+LOG = logging.getLogger(__name__)
 
 if PY3:
     long = int
@@ -24,6 +24,101 @@ try:
     RE_TYPES = (RE_TYPE, Regex)
 except ImportError:
     RE_TYPES = (RE_TYPE,)
+
+
+class DataType(Enum):
+    BOOL = 1
+    LONG = 2
+    DOUBLE = 3
+    STRING = 4
+    DICT = 5
+    DATETIME = 6
+    GEOPOINT = 7
+    UNKNOWN = 8
+
+    def __str__(self):
+        return {
+            DataType.BOOL: "bool",
+            DataType.LONG: "long",
+            DataType.DOUBLE: "double",
+            DataType.STRING: "string",
+            DataType.DICT: "object",
+            DataType.DATETIME: "datetime",
+            DataType.GEOPOINT: "geopoint",
+            DataType.UNKNOWN: ""
+        }.get(self, "")
+
+    @staticmethod
+    def equatable():
+        return [DataType.BOOL, DataType.LONG, DataType.DOUBLE, DataType.STRING, DataType.UNKNOWN]
+
+    @staticmethod
+    def comparable():
+        return [DataType.LONG, DataType.DOUBLE, DataType.DATETIME]
+
+    @staticmethod
+    def baseTypes():
+        return [DataType.BOOL, DataType.LONG, DataType.DOUBLE, DataType.STRING, DataType.DATETIME]
+
+    @staticmethod
+    def fromStr(typeStr):
+        return {
+            "bool": DataType.BOOL,
+            "long": DataType.LONG,
+            "double": DataType.DOUBLE,
+            "string": DataType.STRING,
+            "object": DataType.DICT,
+            "datetime": DataType.DATETIME,
+            "geopoint": DataType.GEOPOINT,
+            "": DataType.UNKNOWN
+        }.get(typeStr, DataType.UNKNOWN)
+
+    def prefix(self):
+        return "" if self == DataType.UNKNOWN else str(self) + "_"
+
+    @staticmethod
+    def dateToStr(value, data_type):
+        return value.isoformat() if data_type == DataType.DATETIME else value
+
+    @staticmethod
+    def isGeoPoint(value):
+        return len(value.keys()) == 2 and 'lat' in value and 'lon' in value
+
+    @staticmethod
+    def typeForDictValue(value):
+        if DataType.isGeoPoint(value):
+            return DataType.GEOPOINT
+        return DataType.DICT
+
+    @staticmethod
+    def forValue(value):
+        """
+        Dict and list handled separately because dict creation tries to execute their handlers while initialization
+        """
+        try:
+            if isinstance(value, dict):
+                return DataType.typeForDictValue(value)
+            elif isinstance(value, list):
+                return DataType.forValue(value[0])
+            else:
+                return {
+                    bool: DataType.BOOL,
+                    int: DataType.LONG,
+                    long: DataType.LONG,
+                    float: DataType.DOUBLE,
+                    str: DataType.STRING,
+                    unicode: DataType.STRING,
+                    datetime: DataType.DATETIME
+                }[type(value)]
+        except:
+            return DataType.UNKNOWN
+
+    @staticmethod
+    def attributeNameFromKey(key):
+        for value_type in DataType:
+            if key.startswith(value_type.prefix()):
+                return key[len(value_type.prefix()):]
+        return key
 
 
 class DocumentFormatter(object):
@@ -104,7 +199,8 @@ class DefaultDocumentFormatter(DocumentFormatter):
     def transform_element(self, key, value):
         try:
             new_value = self.transform_value(value)
-            yield key, new_value
+            new_key = DataType.forValue(value).prefix() + key
+            yield new_key, new_value
         except ValueError as e:
             LOG.warn("Invalid value for key: %s as %s"
                      % (key, str(e)))
@@ -167,7 +263,8 @@ class DocumentFlattener(DefaultDocumentFormatter):
                     transformed = self.transform_element(k, v)
                     for new_k, new_v in transformed:
                         if top_level:
-                            yield new_k, new_v
+                            new_key = DataType.forValue(new_v).prefix + new_k
+                            yield new_key, new_v
                         else:
                             yield "%s.%s" % (path_string, new_k), new_v
         return dict(flatten(document, []))
